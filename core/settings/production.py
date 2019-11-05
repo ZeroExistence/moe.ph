975fbedee1a183b3e204c9baf17d815a48c1166b
ZeroExistence <tonydeguia009@gmail.com>
2019-11-05 16:48:47 +0800

    Updating the settings.py to use separate .local configuration for deployment.

--- core/settings/production.py
+++ core/settings/production.py
@@ -2,6 +2,25 @@ from .base import *
 
 DEBUG = False
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'stream'  : 'ext://sys.stdout',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
+
 try:
     from .local import *
 except ImportError:
